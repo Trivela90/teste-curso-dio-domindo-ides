2185e7fe6ff75ad61b8f70df02fa2fa239d9ece4
Henrique Trivelato <htangelo97@hotmail.com>
2022-05-24 01:34:14 -0300

    Criando class Livro

--- src/br/com/dio/TerceiroPrograma.java
+++ src/br/com/dio/TerceiroPrograma.java
@@ -29,6 +29,9 @@ public class TerceiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro1 = new Livro("O problema dos 3 corpos",300);
+        System.out.println(livro1);
+
 //        int a = 5;
 //        int b = 3;
         /* Neste editor, para criar system.out em Java,
@@ -45,4 +48,38 @@ public class TerceiroPrograma {
     /* Vou selecionar essa frase toda com o cursor
     e apertar Crtl+Shift+/ para comentar ou
      descomentar esse bloco */
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
 }
